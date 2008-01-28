9a9caed0cf0a5943463e68142b4faec1d7e4bfd3
eikeon@eikeon.com <eikeon@eikeon.com@4f84a9ca-5c3e-0410-8a68-0bdf370325d5>
2008-01-28 07:33:47 +0000

    more plugin split
    git-svn-id: http://rdflib.googlecode.com/svn/trunk@1326 4f84a9ca-5c3e-0410-8a68-0bdf370325d5

--- rdflib_plugins/store/MySQL.py
+++ rdflib_plugins/store/MySQL.py
@@ -1136,3 +1136,35 @@ CREATE TABLE %s_namespace_binds (
     uri           text,
     PRIMARY KEY (prefix),
     INDEX uri_index (uri(100))) ENGINE=InnoDB"""        
+
+
+
+from rdflib import Literal, ConjunctiveGraph, Namespace, BNode, URIRef, Literal, plugin
+from rdflib.store import Store, VALID_STORE, CORRUPTED_STORE, NO_STORE, UNKNOWN
+from rdflib.graph import Graph, ConjunctiveGraph
+
+
+def test_dType_encoding():
+    correct=normalizeValue('http://www.w3.org/2001/XMLSchema#integer', 'U')
+    wrong=normalizeValue('http://www.w3.org/2001/XMLSchema#integer', 'L')
+
+    
+    store = plugin.get('MySQL',Store)()
+    store.open('user=..,password=..,db=test,host=..',create=False)
+    Graph(store).add((BNode(),URIRef('foo'),Literal(1)))
+    db=store._db
+    cursor=db.cursor()
+    cursor.execute(
+    "select * from %s where data_type = '%s'"%
+        (store.literalProperties,
+         wrong))
+    assert not cursor.fetchone(),"Datatype encoding bug!"
+    for suffix,(relations_only,tables) in store.viewCreationDict.items():
+        query='create view %s%s as %s'%(store._internedId,
+                                        suffix,
+        ' union all '.join([t.viewUnionSelectExpression(relations_only) 
+                            for t in tables]))
+        print "## Creating View ##\n",query
+    
+    store.rollback()    
+    store.close()
