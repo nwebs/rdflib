0b14002b0c58a59cfc39f7116ee5d9ff615eeb92
eikeon@eikeon.com <eikeon@eikeon.com@4f84a9ca-5c3e-0410-8a68-0bdf370325d5>
2008-01-30 04:23:18 +0000

    added __init__.py for package
    git-svn-id: http://rdflib.googlecode.com/svn/trunk@1367 4f84a9ca-5c3e-0410-8a68-0bdf370325d5

---
+++ test/test_RDFPipe.py
@@ -0,0 +1,16 @@
+import unittest
+
+
+class RDFPipeTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def testA(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+
