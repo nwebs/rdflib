6a5584b9202f08b555b83f9591df2828a48b826f
eikeon@eikeon.com <eikeon@eikeon.com@4f84a9ca-5c3e-0410-8a68-0bdf370325d5>
2008-01-30 04:17:50 +0000

    added setup bits
    git-svn-id: http://rdflib.googlecode.com/svn/trunk@1365 4f84a9ca-5c3e-0410-8a68-0bdf370325d5

---
+++ plugins/setup.py
@@ -0,0 +1,93 @@
+from ez_setup import use_setuptools
+use_setuptools()
+
+from setuptools import setup, find_packages
+
+from distutils.extension import Extension
+
+__version__ = "0.9"
+__date__ = "not/yet/released"
+
+
+setup(
+    name = 'rdflib_plugins',
+    version = __version__,
+    description = "Standard plugins for rdflib.",
+    author = "Daniel 'eikeon' Krech",
+    author_email = "eikeon@eikeon.com",
+    maintainer = "Daniel 'eikeon' Krech",
+    maintainer_email = "eikeon@eikeon.com",
+    url = "http://rdflib.net/",
+    license = "http://rdflib.net/latest/LICENSE",
+    platforms = ["any"],
+    classifiers = ["Programming Language :: Python",
+                   "License :: OSI Approved :: BSD License",
+                   "Topic :: Software Development :: Libraries :: Python Modules",
+                   "Operating System :: OS Independent",
+                   "Natural Language :: English",
+                   ],
+    long_description = \
+    """
+
+    If you have recently reported a bug marked as fixed, or have a craving for
+    the very latest, you may want the development version instead:
+    http://rdflib.googlecode.com/svn/rdflib_plugins/#egg=rdflib-dev
+
+    """,
+    download_url = "http://rdflib.net/rdflib_plugins-%s.tar.gz" % __version__,
+
+    packages = find_packages(),
+
+    install_requires = ['rdflib'],
+
+    ext_modules = [
+        Extension(
+            name='rdflib_plugins.sparql.bison.SPARQLParserc',
+            sources=['src/bison/SPARQLParser.c'],
+            ),
+        ],
+
+    tests_require = ["nose>=0.9.2"],
+
+    test_suite = 'nose.collector',
+
+    entry_points = {        
+        'rdflib.plugins.store': [
+            'IOMemory = rdflib_plugins.store.IOMemory:IOMemory',
+            'Memory = rdflib_plugins.store.Memory:Memory',
+            'Sleepycat = rdflib_plugins.store.Sleepycat:Sleepycat',
+            'BerkeleyDB = rdflib_plugins.store.BerkeleyDB:BerkeleyDB', 
+            'BDBOptimized = rdflib_plugins.store.BDBOptimized:BDBOptimized',
+            'MySQL = rdflib_plugins.store.MySQL:MySQL',
+            'SQLite = rdflib_plugins.store.SQLite:SQLite',
+            'ZODB = rdflib_plugins.store.ZODB:ZODB',
+            'sqlobject = rdflib_plugins.store._sqlobject:SQLObject',
+            'Redland = rdflib_plugins.store.Redland:Redland',
+            ],
+        'rdflib.plugins.serializer': [
+            'rdf =     rdflib_plugins.serializers.XMLSerializer:XMLSerializer',
+            'xml =     rdflib_plugins.serializers.XMLSerializer:XMLSerializer',
+            'rdf/xml =     rdflib_plugins.serializers.XMLSerializer:XMLSerializer',
+            'pretty-xml =     rdflib_plugins.serializers.PrettyXMLSerializer:PrettyXMLSerializer',
+            'nt =     rdflib_plugins.serializers.NTSerializer:NTSerializer',
+            'turtle =     rdflib_plugins.serializers.TurtleSerializer:TurtleSerializer',
+            'n3 =     rdflib_plugins.serializers.N3Serializer:N3Serializer',
+            ],
+        'rdflib.plugins.parser': [
+            'xml =     rdflib_plugins.parsers.RDFXMLParser:RDFXMLParser',
+            'trix =     rdflib_plugins.parsers.TriXParser:TriXParser',
+            'n3 =     rdflib_plugins.parsers.N3Parser:N3Parser',
+            'notation3 =     rdflib_plugins.parsers.N3Parser:N3Parser',
+            'nt =     rdflib_plugins.parsers.NTParser:NTParser',
+            'rdfa =     rdflib_plugins.parsers.RDFaParser:RDFaParser',
+            ],
+        'rdflib.plugins.query_processor': [
+            'sparql = rdflib_plugins.sparql.bison.Processor:Processor',
+            ],
+        'rdflib.plugins.query_result': [
+            'SPARQLQueryResult = rdflib_plugins.sparql.QueryResult:SPARQLQueryResult'
+            ],
+        },
+
+    )
+
