6430fc81b257e937982010690d35d99297859b49
eikeon@eikeon.com <eikeon@eikeon.com@4f84a9ca-5c3e-0410-8a68-0bdf370325d5>
2008-01-30 01:11:49 +0000

    moving into trunk
    git-svn-id: http://rdflib.googlecode.com/svn/trunk@1356 4f84a9ca-5c3e-0410-8a68-0bdf370325d5

---
+++ rdflib_plugins/test/Sleepycat.py
@@ -0,0 +1,26 @@
+import logging
+_logger = logging.getLogger(__name__)
+
+from test.test_graph import GraphTestCase
+
+try:
+    from rdflib.store.Sleepycat import Sleepycat
+    class SleepycatGraphTestCase(GraphTestCase):
+        store_name = "Sleepycat"
+except ImportError, e:
+    _logger.warning("Can not test Sleepycat store: %s" % e)
+
+
+from test.context import ContextTestCase
+
+try:
+    from rdflib.store.Sleepycat import Sleepycat
+    class SleepycatStoreTestCase(ContextTestCase):
+        store = "Sleepycat"
+except ImportError, e:
+    _logger.warning("Can not test Sleepycat store: %s" % e)
+
+
+#class Sleepycat(PychinkoTestCase):
+#    backend = 'Sleepycat'
+
